9e4913bd2ea4ab6db291a773024e0ed1d12831de
MASAMUNELAPTOP\masam <Masamunemh3ds@gmail.com>
2025-11-20 15:28:50 +0900

    Arduinoコード変更

--- Arduino_pressure/Arduino_pressure.ino
+++ Arduino_pressure/Arduino_pressure.ino
@@ -1,15 +1,15 @@
-int sensor_value[16] ;//
+int sensor_value[16];
 
-void setup(){
-  Serial.begin(9600);//serialport open
+void setup() {
+  Serial.begin(9600); // serial port open
 }
 
-void loop(){
-for(int i=0;i<16;i++){
-  sensor_value[i] = analogRead(i);//Ai の値読み込み
-  sensor_value[i] = map(sensor_value[i], 0, 1023, 0, 255);//1 バイト分に変換
-  Serial.write(sensor_value[i]);//シリアルポートにデータ出力
-}
-Serial.print('¥n');
-delay(3000);
+void loop() {
+  for(int i = 0; i < 16; i++) {
+    sensor_value[i] = analogRead(i);               // Ai の値読み込み
+    sensor_value[i] = map(sensor_value[i], 0, 1023, 0, 255); // 1バイトに変換
+    Serial.write(sensor_value[i]);                 // バイトとして送信
+  }
+  Serial.println(); // 改行
+  delay(1000);
 }
